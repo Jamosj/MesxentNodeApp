9e8fdabfbfdceb8dbc4d74e01540212d6c7d6e6a
Your Name <your_github_email@example.com>
2026-01-16 22:41:17 +0100

    Adding MainActivity: The App Brain

---
+++ app/src/main/java/com/mesxent/node/MainActivity.java
@@ -0,0 +1,10 @@
+package com.mesxent.node;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(Bundle savedInstanceState);
+    }
+}
