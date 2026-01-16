e59b75581bdf779ee31b00853a1b90e8f048f16f
Your Name <your_github_email@example.com>
2026-01-16 22:49:37 +0100

    Final Structural Correction

--- app/src/main/java/com/mesxent/node/MainActivity.java
+++ app/src/main/java/com/mesxent/node/MainActivity.java
@@ -1,10 +1,9 @@
 package com.mesxent.node;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(Bundle savedInstanceState);
+        super.onCreate(savedInstanceState);
     }
 }
